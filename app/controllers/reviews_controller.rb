86b4bce564ffa57647a4137f57421a1f045a39c9
Tony <a-traina2018@nlaw.northwestern.edu>
2018-03-08 15:08:04 -0600

    still working but here we go

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,12 +17,14 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    movie_id = Movie.find_by(id: params["id"])
     review = Review.new
+    movie = Movie.find_by(id: params["id"])
     review.movie_id = params["movie_id"]
     review.rating = params["rating"]
     review.content = params["content"]
     review.save
-    redirect_to "/reviews"
+    redirect_to "/movies/#{movie_id}"
   end
 
   def edit
